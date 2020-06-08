c4053c36e1622342ba1ddaa9ca5f40b55be6833f
Данила Лужин <dluzhin@nvg.ru>
2020-06-08 14:13:57 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+/*VIEWS*/
+import { HomeComponent} from './views/home/home.component';
+import { ElementsComponent } from './views/elements/elements.component';
+import { ComponentsComponent } from './views/components/components.component';
+import { TypographyComponent } from './views/typography/typography.component';
+import { PagesComponent } from './views/pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'elements',
+    component: ElementsComponent
+  },
+  {
+    path: 'components',
+    component: ComponentsComponent
+  },
+  {
+    path: 'typography',
+    component: TypographyComponent
+  },
+  {
+    path: 'pages',
+    component: PagesComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { SvgSpriteComponent } from './shared/components/svg-sprite/svg-sprite.component';
 import { NavigationComponent } from './core/navigation/navigation.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { TypographyComponent } from './views/typography/typography.component';
+import { HomeComponent } from './views/home/home.component';
+import { ElementsComponent } from './views/elements/elements.component';
+import { ComponentsComponent } from './views/components/components.component';
+import { PagesComponent } from './views/pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SvgSpriteComponent,
     NavigationComponent,
-    FooterComponent
+    FooterComponent,
+    TypographyComponent,
+    HomeComponent,
+    ElementsComponent,
+    ComponentsComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
